ba9b7032ffb98f09eeb0e04d300ddb75085bdaf1
Sisi Vieira <cicy886@gmail.com>
2022-03-08 21:10:51 -0800

    Successfully add a navbar with a dropdown menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,41 @@
 import React from "react";
 import CardSpreadControl from "./CardSpreadControl";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./Header";
+// import Header from "./Header";
 import CardControl from "./CardControl";
 import CategoryControl from "./CategoryControl";
+import Navbar from "./Navbar";
+import Wands from "./Wands";
+import Cups from "./Cups";
+import Swords from "./Swords";
+import Pentacles from "./Pentacles";
 
 function App() {
   return (
     <Router>
-      <Header />
+      <Navbar />
       <Switch>
-        <Route path="/category">
+        <Route path="/wands">
+          <Wands />
+        </Route>
+        <Route path="/cups">
+          <Cups />
+        </Route>
+        <Route path="/swords">
+          <Swords />
+        </Route>
+        <Route path="/pentacles">
+          <Pentacles />
+        </Route>
+        <Route path="/categories">
           <CategoryControl />
         </Route>
-        <Route path="/cardSpread">
+        <Route path="/card-spread">
           <CardSpreadControl />
         </Route>
         <Route path="/">
           <CardControl />
         </Route>
-        
       </Switch>
     </Router>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
-// import { Navbar, Container } from "react-bootstrap";
+// import React from "react";
+// import { Link } from "react-router-dom";
+// // import { Navbar, Container } from "react-bootstrap";
 
-const Header = () => {
-  return (
-    <React.Fragment>
-      <h1>Tarot Reader</h1>
-      <div className="row">
-        <div className="column">
-          <Link to="/">Home</Link>
-        </div>
-        <div className="column">
-          <Link to="/cardSpread">Card Spread</Link>
-        </div>
-        <div className="column">
-          <Link to="/category">Category</Link>
-        </div>
-      </div>
-    </React.Fragment>
-  );
-};
+// const Header = () => {
+//   return (
+//     <React.Fragment>
+//       <h1>Tarot Reader</h1>
+//       <div className="row">
+//         <div className="column">
+//           <Link to="/">Home</Link>
+//         </div>
+//         <div className="column">
+//           <Link to="/cardSpread">Card Spread</Link>
+//         </div>
+//         <div className="column">
+//           <Link to="/category">Category</Link>
+//         </div>
+//       </div>
+//     </React.Fragment>
+//   );
+// };
 
-export default Header;
+// export default Header;
